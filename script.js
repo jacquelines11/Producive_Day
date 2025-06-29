753c1373a588e5432513421a673cdd6b9b1f78a2
Jack <86537081+jacquelines11@users.noreply.github.com>
2025-06-29 00:04:23 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const blueButton = document.getElementById('blueButton');
+const pinkButton = document.getElementById('pinkButton');
+
+blueButton.addEventListener('click', function() {
+  document.body.style.backgroundColor = '#C5EAFA';
+});
+
+pinkButton.addEventListener('click', function() {
+  document.body.style.backgroundColor = '#FFD1DC';
+});
